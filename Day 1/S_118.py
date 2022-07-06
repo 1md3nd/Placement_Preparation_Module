a7c336776c6f3f93ca655cf3dd4adc88a38c989e
ANURAG RANJAN <61619621+1md3nd@users.noreply.github.com>
2022-07-06 16:42:19 +0530

    Day 1

---
+++ Day 1/S_118.py
@@ -0,0 +1,30 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        col = 0
+        tri = []
+        for i in range(numRows):
+            col +=1
+            temp = []
+            for j in range(col):
+                temp.append(0)
+            tri.append(temp)
+        if numRows == 1:
+            tri[0][0] = 1
+            return tri
+        if numRows == 2:
+            tri[0][0] = 1
+            tri[1][0] = 1
+            tri[1][1] = 1
+            return tri
+        else:
+            tri[0][0] = 1
+            tri[1][0] = 1
+            tri[1][1] = 1
+            ext = 0
+            for i in range(2,numRows):
+                ext +=1
+                tri[i][0] = 1
+                for j in range(ext):
+                    tri[i][j+1] = tri[i-1][j]+tri[i-1][j+1]
+                tri[i][j+2] = 1
+            return tri
---
+++ Day 1/S_73.py
@@ -0,0 +1,17 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        row = []
+        col = []
+        row_len = len(matrix)
+        col_len = len(matrix[0])
+        for i in range(row_len):
+            for j in range(col_len):
+                if matrix[i][j] == 0:
+                    row.append(i)
+                    col.append(j)
+        for i in row:
+            for j in range(col_len):
+                matrix[i][j] = 0
+        for j in col:
+            for i in range(row_len):
+                matrix[i][j] = 0
