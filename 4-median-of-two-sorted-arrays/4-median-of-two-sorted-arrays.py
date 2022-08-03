096e262fde824fa8bfce123ed68b33e02698a502
ANURAG RANJAN <61619621+1md3nd@users.noreply.github.com>
2022-08-03 23:19:23 +0530

    Time: 269 ms (5.09%), Space: 14.1 MB (68.30%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -6,34 +6,27 @@ class Solution:
         max_heap = []
         temp_heap = []
         def Fix_Heap(x):
-            if len(max_heap) == 0 and len(min_heap) == 0:
-                heapq.heappush(min_heap, -x)
+            if len(max_heap) == 0 or abs(max_heap[0]) > x:
+                heapq.heappush(max_heap, -x)
             else:
-                if x <= -min_heap[0]:
-                    heapq.heappush(min_heap, -x)
-                else:
-                    heapq.heappush(max_heap, x)
-            if len(min_heap) > len(max_heap) + 1:
-                heapq.heappush(max_heap,-(heapq.heappop(min_heap)))
-            elif len(min_heap) < len(max_heap):
+                heapq.heappush(min_heap, x)
+                
+            if len(max_heap) > len(min_heap) + 1:
                 heapq.heappush(min_heap,-(heapq.heappop(max_heap)))
+            elif len(max_heap) < len(min_heap):
+                heapq.heappush(max_heap,-(heapq.heappop(min_heap)))
         
         def Find_Median():
             if len(max_heap) == len(min_heap):
-                return -min_heap[0]/2 + max_heap[0]/2
-            elif len(max_heap) > len(min_heap):
-                return max_heap[-1]
+                return -(heapq.heappop(max_heap))/2 + heapq.heappop(min_heap)/2
             else:
-                return -min_heap[0]
+                return -heapq.heappop(max_heap)
         for i in nums1:
             heapq.heappush(temp_heap,i)
         for i in nums2:
             heapq.heappush(temp_heap,i)
-        print(temp_heap)
         while temp_heap:
             Fix_Heap(heapq.heappop(temp_heap))
-            # print(max_heap,min_heap)
-        
         return Find_Median()
     
         
