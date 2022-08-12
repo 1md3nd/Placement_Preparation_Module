e1806250ef2482d7c6d35f0efd2245cc13b8bc0b
ANURAG RANJAN <61619621+1md3nd@users.noreply.github.com>
2022-08-13 00:07:02 +0530

    Time: 720 ms (77.76%), Space: 14 MB (69.83%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -1,14 +1,8 @@
 class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
         n = len(arr)
-        dp = [-1] * n
-        def helper(index):
-            nonlocal dp
-            nonlocal n
-            if index == n:
-                return 0
-            if dp[index] != -1:
-                return dp[index]
+        dp = [0] * (n+1)
+        for index in range(n-1,-1,-1):
             summ = 0
             count = 0
             INF = 10 ** 20
@@ -17,8 +11,7 @@ class Solution:
             for j in range(index,min(index+k,n)):
                 count +=1
                 mxa = max(mxa,arr[j])
-                summ = count * mxa + helper(j + 1)
+                summ = count * mxa + dp[j+1]
                 MaxAns = max(summ,MaxAns)
             dp[index] = MaxAns
-            return MaxAns
-        return helper(0)
+        return dp[0]
