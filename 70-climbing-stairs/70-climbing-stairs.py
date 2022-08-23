de8f86c3bed4cf52675286a6893e4ddc9c919a3f
ANURAG RANJAN <61619621+1md3nd@users.noreply.github.com>
2022-08-24 00:21:39 +0530

    Time: 45 ms (55.20%), Space: 13.8 MB (57.48%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,5 +1,16 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+#### To Print all thecombinations
+        # def comb(num,res):
+        #     if num < 0:
+        #         return 
+        #     if num == 0:
+        #         print(res)
+        #         return
+        #     comb(num-1,res + "1")
+        #     comb(num-2,res + "2")
+        # comb(n,"")
+#### To count pattern
         x_ = 1
         y_ = 1
         for _ in range(n-1):
