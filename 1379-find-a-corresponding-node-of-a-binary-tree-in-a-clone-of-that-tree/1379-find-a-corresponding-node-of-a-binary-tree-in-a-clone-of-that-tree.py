742045f5e2d27b3197af9e126372e59ffa65acfd
ANURAG RANJAN <61619621+1md3nd@users.noreply.github.com>
2022-09-02 14:45:36 +0530

    Time: 1497 ms (5.09%), Space: 24.1 MB (37.40%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
@@ -7,16 +7,33 @@
 
 class Solution:
     def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
-        head = cloned
-        q = []
-        q.append(head)
-        while q:
-            size = len(q)
-            for _ in range(size):
-                head = q.pop(0)
-                if head.val == target.val:
-                    return head
-                if head.left:
-                    q.append(head.left)
-                if head.right:
-                    q.append(head.right)
+        # head = cloned
+        "BFS"
+        # q = []
+        # q.append(head)
+        # while q:
+        #     size = len(q)
+        #     for _ in range(size):
+        #         head = q.pop(0)
+        #         if head.val == target.val:
+        #             return head
+        #         if head.left:
+        #             q.append(head.left)
+        #         if head.right:
+        #             q.append(head.right)
+        "DFS"
+        res = TreeNode(0)
+        def dfs(head,target):
+            nonlocal res
+            # if head is None:
+            #     return
+            # print(head.val)
+            if head.val == target.val:
+                res = head
+                return
+            if head.left:
+                dfs(head.left,target)
+            if head.right:
+                dfs(head.right,target)
+        dfs(cloned,target)
+        return res
