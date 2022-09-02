1ee37279eb1a02316362920dad2ffa77dfd924d5
ANURAG RANJAN <61619621+1md3nd@users.noreply.github.com>
2022-09-02 16:19:50 +0530

    Time: 51 ms (47.68%), Space: 13.8 MB (89.28%) - LeetHub

--- 897-increasing-order-search-tree/897-increasing-order-search-tree.py
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.py
@@ -6,7 +6,11 @@
 #         self.right = right
 class Solution:
     def increasingBST(self, root: TreeNode) -> TreeNode:
-        q = []
+        """
+        Without extra space
+        Time -> O(logn)
+        Space -> O(1) + O(n) (for new TreeNode)
+        """
         new = TreeNode()
         head = new
         def dfs(node):
@@ -18,11 +22,27 @@ class Solution:
             new = new.right
             dfs(node.right)
         dfs(root)
+        return head.right
+    
+        """
+        With extra Space
+        Time -> O(logn)
+        x -> Number of not null element
+        Space -> O(x) + O(n) (For new TreeNode)
+            """
+        # q = []
+        # def dfs(node):
+        #     if node is None:
+        #         return
+        #     dfs(node.left)
+        #     q.append(node.val)
+        #     new = new.right
+        #     dfs(node.right)
+        # dfs(root)
         # new = TreeNode(q.pop(0))
         # head = new
         # while q:
         #     new.right = TreeNode(q.pop(0))
         #     new = new.right
         # return head
-        return head.right
     
