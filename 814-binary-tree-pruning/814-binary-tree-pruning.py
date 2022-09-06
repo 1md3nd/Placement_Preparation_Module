3f3f1a3161a51ac58a17df8de102eaca6890df38
ANURAG RANJAN <61619621+1md3nd@users.noreply.github.com>
2022-09-06 17:14:10 +0530

    Time: 53 ms (42.07%), Space: 13.8 MB (68.92%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.py
+++ 814-binary-tree-pruning/814-binary-tree-pruning.py
@@ -7,23 +7,33 @@
 class Solution:
     def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         head = TreeNode(-1,root)
-        def dfs(node):
-            if node is None:
-                return False
-            flag = False
-            if node.val == 1:
-                flag = True
-            if not dfs(node.left):
-                node.left = None
-            else:
-                flag = True
-            if not dfs(node.right):
-                node.right = None
-            else:
-                flag = True
-            return flag 
+#         def dfs(node):
+#             if node is None:
+#                 return False
+#             flag = False
+#             if node.val == 1:
+#                 flag = True
+#             if not dfs(node.left):
+#                 node.left = None
+#             else:
+#                 flag = True
+#             if not dfs(node.right):
+#                 node.right = None
+#             else:
+#                 flag = True
+#             return flag 
         
-        dfs(head)
-        # print(root)
+#         dfs(head)
+#         # print(root)
+#         return head.left
+        def check(node):
+            if node is None:
+                return None
+            node.left = check(node.left)
+            node.right = check(node.right)
+            if not node.left and not node.right and node.val == 0:
+                return None
+            return node
+        check(head)
         return head.left
             
