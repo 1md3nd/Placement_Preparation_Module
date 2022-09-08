8e725d90dbb60bb1734647188f80ea8825261e18
ANURAG RANJAN <61619621+1md3nd@users.noreply.github.com>
2022-09-09 00:15:58 +0530

    Time: 667 ms (12.25%), Space: 15.2 MB (34.77%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        hash_ = collections.Counter(p)
+        j = 0
+        res = []
+        h2 = {}
+        for i in range(len(s)):
+            if s[i] not in hash_:
+                j = i+1
+                h2 = {}
+                continue
+                
+            elif s[i] in h2:
+                h2[s[i]] +=1
+                while h2[s[i]] > hash_[s[i]]:
+                    h2[s[j]] -=1
+                    j +=1
+            else:
+                h2[s[i]] = 1
+            print(h2)
+
+            if h2 == hash_:
+                res.append(j)
+                h2[s[j]]-=1
+                j +=1
+        return res
+            
+            
